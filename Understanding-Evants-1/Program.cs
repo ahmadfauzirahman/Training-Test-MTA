80b9ce247914143f5816dea1e018122e1f727011
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-20 00:02:54 +0700

    MTA TRAINING C#

---
+++ Understanding-Evants-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+
+namespace Understanding_Evants_1
+{
+    public class Metronome
+    {
+        public event TrickHandler Tick;
+        public EventArgs e = null;
+
+        public delegate void TrickHandler(Metronome m, EventArgs e);
+
+        public void Start()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(3000);
+                if (Tick != null)
+                {
+                    Tick(this, e);
+                }
+            }
+        }
+    }
+
+    public class Listener
+    {
+        public void Subscriber(Metronome m)
+        {
+            m.Tick += new Metronome.TrickHandler(HeaderIt);
+        }
+
+        private void HeaderIt(Metronome m, EventArgs e)
+        {
+            Console.WriteLine("Heard IT");
+        }
+    }
+        internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Metronome m = new Metronome();
+            Listener i = new Listener();
+            i.Subscriber(m);
+            m.Start();
+        }
+    }
+}
