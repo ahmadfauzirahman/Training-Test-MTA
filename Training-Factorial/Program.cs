cad5c275cddfde76f822830120a675a376084e27
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:47:41 +0700

    MTA TRAINING C#

---
+++ Training-Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training_Factorial
+{
+    internal class Program
+    {
+        public static int Factorial(int n)
+        {
+            if (n > 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int fauzi =Factorial(2);
+            Console.WriteLine(fauzi);
+        }
+    }
+}
