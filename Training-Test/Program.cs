ab932b1a410d600d656103029ea274f90d79cd01
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:48:12 +0700

    MTA TRAINING C#

---
+++ Training-Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training_Test
+{
+    internal class Program
+    {
+        static int i = 5;
+        static void Func(int i)
+        {
+            i = 10;
+        }
+        public static void Main(string[] args)
+        {
+            string one = "sololearn";
+            string two = one;
+            string three = one.Remove(2);
+            Console.WriteLine(three);
+            string four = two.Substring(4);
+            Console.Write(three+four);
+            
+            Console.WriteLine("========================");
+            Test t = new Test();
+            Console.Write(t.a);
+            Console.WriteLine();
+            Console.WriteLine("=====================");
+            Console.Write(i);
+            
+        }
+
+
+        public class Test
+        {
+            public int a = 5;
+
+            public Test()
+            {
+                int a = 3;
+                a++;
+            }
+        }
+    }
+}
