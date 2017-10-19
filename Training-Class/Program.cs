f23c71c77c12548f82b80bb7fd52e8559e1ef02e
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:50:25 +0700

    MTA TRAINING C#

---
+++ Training-Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training_Class
+{
+    internal class Program
+    {
+        private double length;
+        private double width;
+
+        public void Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
