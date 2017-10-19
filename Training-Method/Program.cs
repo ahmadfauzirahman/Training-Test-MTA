4146149692a84df95b24acfd6131136638d05687
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:47:14 +0700

    MTA TRAINING C#

---
+++ Training-Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training_Method
+{
+    internal class Program
+    {
+        public static void jumlah(int var1, ref int var2)
+        {
+            var1 = var1 + 5;
+            var2 = var2 + 5;
+        }
+        
+        public static void Main(string[] args)
+        {
+            int var1 = 5;
+            int var2 = 6;
+            jumlah(var1, ref var2);
+            Console.WriteLine("Nilai Var 1  = {0}",var1);
+            Console.WriteLine("Nilai Var 2  = {0}",var2);
+        }
+    }
+}
