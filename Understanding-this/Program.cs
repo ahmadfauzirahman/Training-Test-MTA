6f5f830bded147342de1f5bdd02ac2fc075f9cd9
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:40:20 +0700

    MTA TRAINING C#

---
+++ Understanding-this/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Understanding_this
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
