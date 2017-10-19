8bfb093455f7ac0767f2764548c649beeceb5ccf
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:49:13 +0700

    MTA TRAINING C#

---
+++ Understending-Properties-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Understending_Properties_2
+{
+    public class Employee
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    
+    // Pewarisan
+    public class Manager : Employee
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value + " ,Manager"; }
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Manager n1 = new Manager();
+            n1.Name = "Fauzi";
+            ((Employee) n1).Name = "Fitriani";
+            
+            Console.WriteLine("Name in the derived class is {0}" , n1.Name);
+            Console.WriteLine("Name in the BASE class is {0}" , ((Employee)n1).Name);
+        }
+    }
+}
