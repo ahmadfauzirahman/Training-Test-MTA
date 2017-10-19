7440bed8b98e504f61092e042f6ae2fafe0267d6
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:48:59 +0700

    MTA TRAINING C#

---
+++ Understending-Properties/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Understending_Properties
+{
+
+    public class Employee
+    {
+        public static int NumberOfEmployess;
+        private static int counter;
+        private string name;
+        
+        //  a read and write intance property     
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        // a read-only static property
+        public static int Counter
+        {
+            get { return counter; }
+        }
+        
+        // A construtor
+
+        public Employee()
+        {
+            counter = ++counter + NumberOfEmployess;
+        }
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee.NumberOfEmployess = 107;
+            Employee e1 = new Employee();
+            e1.Name = "Ahmad Fauzi Rahman";
+            
+            Console.WriteLine("Employe number : {0}", Employee.Counter);
+            Console.WriteLine("Employe name : {0}", e1.Name);
+        }
+    }
+}
