b38b131f47d3a7a54bba4442965c388a7df34ad6
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 21:48:29 +0700

    MTA TRAINING C#

---
+++ Trainning-Test-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trainning_Test_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int y = 3 , result=2;
+            for (int i = 0; i < y; i++)
+            {
+                result++;
+            }
+            Console.WriteLine(result);
+            Console.WriteLine("=================================");
+             Console.WriteLine("RectangleHandler");
+            int[] numbers = {1, 2, 3, 4};
+            int val = numbers[1];
+            Console.WriteLine(val);
+            Console.WriteLine("=================================");
+        }
+    }
+}
