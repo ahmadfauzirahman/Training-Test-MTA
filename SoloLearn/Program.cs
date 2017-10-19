2411296e7ce13a53090bd34291dcc673d3b0f5c3
ojikdev <ahmadfauzirahman99@gmail.com>
2017-10-19 23:41:25 +0700

    MTA TRAINING C#

---
+++ SoloLearn/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace SoloLearn
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = {5, 6, 10};
+            int q = a.Max();
+            Console.WriteLine(q);
+            int s = a.Sum();
+            Console.WriteLine(a);
+            int n = a.Max() + a.Sum() % 10;
+            
+            Console.Write(n);
+            
+            Console.WriteLine("=======================================");
+            Console.WriteLine("=======================================");
+            Console.WriteLine("=======================================");
+
+//            try
+//            {
+//
+//                int a;
+//                int b = 15;
+//                a = (int) b + 20;
+//                Console.Write(b);
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine(e);
+//                
+//            } 
+        }
+    }
+}
